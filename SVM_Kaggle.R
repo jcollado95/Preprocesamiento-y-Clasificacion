b9e1306f48dda5b803d48bae7f6bce2193a1079d
jcm00068 <jcm00068@red.ujaen.es>
2019-01-22 22:43:16 +0100

    Prueba de normalizacion de variables

--- SVM_Kaggle.R
+++ SVM_Kaggle.R
@@ -1,6 +1,22 @@
 # Carga de datos
-datos = read.csv("./datos/train.csv", na.strings = c(" ", "NA", "?"))
+train = read.csv("./datos/train.csv", na.strings = c(" ", "NA", "?"))
 test = read.csv("./datos/test.csv", na.strings = c(" ", "NA", "?"))
+
+datos = train
+
+# Imputacion de datos perdidos
+cat("Datos completos: ", ncc(datos), " e incompletos: ", nic(datos), "\n")
+imputados <- mice(datos)
+datos <- complete(imputados)
+
+# TO DO: Normalize data (Test?)
+#datosPre <- caret::preProcess(datos[,1:50],method=c("center","scale"))
+#datosTransformados <- predict(datosPre,datos[,1:50])
+#datos[ , -51] <- datosTransformados
+
+#testPre <- caret::preProcess(test,method=c("center","scale"))
+#test <- predict(testPre, test)
+
 
 # Filtrado de variables muy correladas
 library(corrplot)
@@ -13,24 +29,18 @@ corrplot::corrplot(corrMatrix,
                    tl.str = 45) # FPC ordena de izquierda a derecha las mas correladas
 
 altaCorrelacion = caret::findCorrelation(corrMatrix, cutoff = 0.8)
-datos.filtrados = datos[ , -altaCorrelacion]
+datos = datos[ , -altaCorrelacion]
 
-# Transformacion
-completos <- ncc(datos.filtrados)
-incompletos <- nic(datos.filtrados)
-cat("Datos completos: ",completos, " e incompletos: ",incompletos,"\n")
-
-imputados <- mice(datos.filtrados)
-datos.imputados <- complete(imputados)
+# TO DO: tune best parameters (C and Gamma) -> tune.svm()
 
 # Generacion del modelo con SVM
 library(e1071)
-model = e1071::svm(C ~ ., data = datos.imputados, type = "C-classification")
+model = e1071::svm(C ~ ., data = datos, type = "C-classification")
 
 # Generacion de la prediccion
 pred = predict(model, test)
 
-# Guardar el archivo para Kaggle
+# Guarda el archivo para Kaggle
 ids = 1:nrow(test)
 res = data.frame(ids, pred)
 colnames(res) = c("Id", "Prediction")
